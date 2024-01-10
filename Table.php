e649e6ed58b9edfbb31fe98a32be3dbbb34b2d62
Sudaniaarvi <155125064+Sudaniaarvi@users.noreply.github.com>
2024-01-10 11:14:08 +0530

    Add files via upload

---
+++ Table.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<body>
+
+<form method="get" action="Table1.php">
+
+Enter number for start: <input type="text" name="sno"><br>
+
+Enter number for end: <input type="text" name="lno"><br>
+
+<input type="submit" name="submit">
+
+</form>
+</body>
+</head>
+</html>
